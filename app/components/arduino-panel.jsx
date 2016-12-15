bbcb09676878e68f0d6a58a39680732712c83453
Riven <4771215@qq.com>
2016-12-15 21:45:27 +0800

    add ACE editor to panel

--- app/components/arduino-panel.jsx
+++ app/components/arduino-panel.jsx
@@ -1,5 +1,9 @@
 const React = require('react');
 import {Button} from 'react-bootstrap';
+import brace from 'brace';
+import AceEditor from 'react-ace';
+import 'brace/mode/java';
+import 'brace/theme/eclipse';
 
 class ArduinoPanelComponent extends React.Component {
     render() {
@@ -24,7 +28,13 @@ class ArduinoPanelComponent extends React.Component {
                 <Button style={{marginLeft:5}}>Upload</Button>
                 <Button style={{float:'right'}}>Open with arduino</Button>
             </div>
-
+            <AceEditor
+                style={{top:45,left:2,height:450}}
+                mode="c_cpp"
+                theme="eclipse"
+                name="arduino-code"
+                editorProps={{$blockScrolling: true}}
+            />,
             </div>
         );
     }
